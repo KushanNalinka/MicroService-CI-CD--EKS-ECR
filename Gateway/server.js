675e43637da72bf33a693921e967f31fd5461a16
Kushan Nalinka <kushannalinka572@gmail.com>
2025-04-30 23:57:50 +0530

    Microservice Impl
    Microservice Impl

---
+++ Gateway/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+// Proxy each path to its service's public IP + port
+// app.use("/s1", proxy("http://<IP_OF_it21310546>:5000"));
+// app.use("/s2", proxy("http://<IP_OF_it21311772>:5000"));
+// app.use("/s3", proxy("http://<IP_OF_it21467448>:5000"));
+// app.use("/s4", proxy("http://<IP_OF_it21894510>:5000"));
+// // Optional root health-check
+// app.get("/", (req, res) => res.send("Gateway is up"));
+
+
+// app.listen(PORT, () => {
+//   console.log(`Gateway is Listening to Port ${PORT}`);
+// });
+
+// simple health-check
+app.get('/health', (_req, res) => res.send('OK'));
+
+// proxy rules
+app.use('/service1', createProxyMiddleware({ target: 'http://it21310546-service:8001', changeOrigin: true }));
+app.use('/service2', createProxyMiddleware({ target: 'http://it21311772-service:8002', changeOrigin: true }));
+app.use('/service3', createProxyMiddleware({ target: 'http://it21467448-service:8003', changeOrigin: true }));
+app.use('/service4', createProxyMiddleware({ target: 'http://it21894510-service:8004', changeOrigin: true }));
+
+app.listen(process.env.PORT || 80, () => {
+  console.log('Gateway listening');
+});
